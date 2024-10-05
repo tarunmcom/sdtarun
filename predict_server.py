8b82d5f867952bbf8040c0b419be57419b13136b
Tarun Mishra <er.tarunmishra@gmail.com>
2024-10-06 01:45:14 +0530

    Update predict_server.py

--- predict_server.py
+++ predict_server.py
@@ -408,7 +408,7 @@ def get_busy_status():
 
 @app.route('/')
 def index():
-    return jsonify({"message": "alive"})  # Updated to return JSON response
+    return jsonify({"message": "alive", "server_type":"Predict"})  # Updated to return JSON response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
